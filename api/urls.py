87424fc5b4827851cdce9b45acd92f0060d0697a
azmatntc <azmatntc@gmail.com>
2026-04-08 21:26:26 +0500

    your message

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('projects', views.get_projects, name='get_projects'),
-    path('leads', views.capture_lead, name='capture_lead'),
-    path('chat', views.chat_proxy, name='chat_proxy'),
-]
--- src/Test.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  console.log('Test.svelte script executing...');
-</script>
-
-<h1 style="color: red; font-size: 48px;">SVELTE IS WORKING!</h1>
